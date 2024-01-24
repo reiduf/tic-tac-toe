a3487e65de613ec484da6c4bfda2b91263500842
reina-duf <reina.dufrene@gmail.com>
2024-01-23 17:06:15 -0700

    adds types for square state and value prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,36 @@
 import { useState } from "react"
 
-// interface SquareProps {
-//   value: string,
-// }
+// TypeScript Stuff
+type Squares = null | 'X' | 'O'
 
-function Square() {
-  type Value = null | 'X' | 'O';
-  const [value, setValue] = useState<Value>(null);
+interface SquareProps {
+  value: Squares,
+}
 
-  function handleClick() {
-    setValue('X')
-  }
-
-  return <button className="square" onClick={handleClick}>{value}</button>
+// Components
+function Square({ value }: SquareProps) {
+  return <button className="square">{value}</button>
 }
 
 
 export default function Board() {
+  const [squares, setSquares] = useState<Squares[]>(Array(9).fill(null)) //[null,null,null,null,null,null,null,null,null]
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
     </>
   );
